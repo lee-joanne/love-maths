71ed7a4d6eadd9c3b2f927767304379c7876d09e
Joanne <lee.joanne54@gmail.com>
2022-01-12 17:09:12 +0000

    Add interactions for javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish loading before running the game
+// Get the bottom elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -27,5 +45,12 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }
+
+/* for challenge
+function displayDivisionQuestion(){
+
+}
+
+*/
