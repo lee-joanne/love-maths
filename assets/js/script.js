9da0c945e32ada6630095d08969230d232292a65
Joanne <lee.joanne54@gmail.com>
2022-01-12 20:44:12 +0000

    Create increment score and wrong answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,8 +61,10 @@ function checkAnswer(){
 
     if (isCorrect) {
         alert("Hey! You got it right! :)")
+        incrementScore();
     } else {
         alert(`Sorry! You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -88,15 +90,25 @@ function calculateCorrectAnswer(){
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}, Aborting!`;
     }
-
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore(){
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+    
+
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
