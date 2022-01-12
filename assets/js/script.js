5daf40ddf8914478a57a2c8a452ddc73b8ce86bc
Joanne <lee.joanne54@gmail.com>
2022-01-12 20:36:59 +0000

    Check Answer created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 // Wait for the DOM to finish loading before running the game
 // Get the bottom elements and add event listeners to them
 
+//Listen to the event of having the entire HTML page loaded. when it loads, do this function
 document.addEventListener("DOMContentLoaded", function() {
+    //We are gathering all of the buttons on the HTML page
     let buttons = document.getElementsByTagName('button');
 
+    //There are multiple buttons so we need to loop through them to individualize them. hence, 'buttons -> button'.
+    // when we have all the individual buttons laid out in the for loop, it will listen for a click.
     for (let button of buttons){
         button.addEventListener("click", function() {
+            // when single button is clicked, the data-type of the HTML will be read. if it is submit, the submit
+            //alert will come up
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
+                checkAnswer();
+            //if data type read from HTML is not submit, it will call the function "runGame" and it will assign
+            // a variable called gameType to the clicked individual button. the data type from HTML of the button is inserted
+            // as the variable
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -29,20 +38,56 @@ function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    // if the button 'addition' was clicked, the data type of addition button was inserted into the variable gameType
+    // as described earlier. So here, if the variable is 'addition' (inserted from the button click), the addition
+    // game function will run. otherwise
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
-
 }
 
+/**
+ * checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer(){
+    
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :)")
+    } else {
+        alert(`Sorry! You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus, etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer(){
+
+    //we are converting text from operand 1 and 2 into integers for maths.
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    //so if the operator is '+', we know it is an addition question.
+    if (operator === '+'){
+        // return the HTML id's where num 1 and num 2 is, and the data type of addition.
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}, Aborting!`;
+    }
 
 }
 
